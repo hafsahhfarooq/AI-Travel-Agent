aee4a77100115a3411446bb9724c0afb8922c6ab
Hafsah Farooq <137312503+hafsahhfarooq@users.noreply.github.com>
2025-02-23 22:57:15 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-SERP_API_KEY = "74ce647d8ada87e81acaeb15e98c827dfe4bee11c2a33f6f9b2be89ba0f833a0"
-weather_api_key = "37c6a384ba314fe9b4683521251802"
-flights_secret_key = "74ce647d8ada87e81acaeb15e98c827dfe4bee11c2a33f6f9b2be89ba0f833a0"
-RAPIDAPI_KEY = "ba0ce8def5msh7c15bfa9bcaeadfp11ecabjsnb8caed196b1e"
-OPENAI_API_KEY = "sk-abcd1234efgh5678abcd1234efgh5678abcd1234"
+SERP_API_KEY = ""
+weather_api_key = ""
+flights_secret_key = ""
+RAPIDAPI_KEY = ""
+OPENAI_API_KEY = ""
 if not OPENAI_API_KEY:
     raise ValueError("Missing OPENAI_API_KEY in environment variables")
